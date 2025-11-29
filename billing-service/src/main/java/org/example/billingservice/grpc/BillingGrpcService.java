11b5f90d5594a12a444aa5de20bfad55aab57b80
Sandula Naveen <sandula.navin@gmail.com>
2025-11-29 22:38:15 +0530

    grpc server added

---
+++ billing-service/src/main/java/org/example/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,27 @@
+package org.example.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    @Override
+    public  void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+        log.info("createBillingAccount request received : {}", billingRequest.toString());
+
+        // BUSINESS LOGIC NEED TO BE ADDED
+        BillingResponse response = BillingResponse
+                                                .newBuilder()
+                                                .setAccountId("12345")
+                                                .setStatus("ACTIVE")
+                                                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
--- patient-service/src/main/java/org/example/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/org/example/patientservice/PatientServiceApplication.java
@@ -1,6 +1,5 @@
 package org.example.patientservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PatientServiceApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         SpringApplication.run(PatientServiceApplication.class, args);
     }
 
