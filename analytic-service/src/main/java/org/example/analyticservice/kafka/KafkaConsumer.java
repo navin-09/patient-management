a267e1f872afb92be4311baa1731d567139ee3cd
Sandula Naveen <sandula.navin@gmail.com>
2025-11-30 23:58:34 +0530

    kafka consumer  added and fixed kafka config

---
+++ analytic-service/src/main/java/org/example/analyticservice/kafka/KafkaConsumer.java
@@ -0,0 +1,22 @@
+package org.example.analyticservice.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            log.info("Received patient event: {}", patientEvent);
+        } catch (InvalidProtocolBufferException e) {
+            log.error("error while parsing/deseralizing  event", e);
+        }
+    }
+}
