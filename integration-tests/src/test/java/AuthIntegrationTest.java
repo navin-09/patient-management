bf890e4629bbf06edff4ac6dc916df295128b0fd
Sandula Naveen <sandula.navin@gmail.com>
2025-12-06 15:15:00 +0530

    Iac added and api-gateway changes

---
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,42 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class AuthIntegrationTest {
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnOkWithValidToken() {
+        // Arrange
+        // Act
+        // Assert
+        String loginPayload = """
+                {
+                    "email":"testuser@test.com",
+                    "password":"password123"
+                }
+                """;
+
+        Response response = given()
+                .contentType("application/json")
+                .body(loginPayload)
+//                .log().all()
+                .when()
+                .post("/auth/login")
+                .then()
+//                .log().all()
+                .statusCode(200)
+                .body("token", notNullValue())
+                .extract().response();
+
+        System.out.println("Generated Token: " + response.jsonPath().getString("token"));
+
+    }
+}
